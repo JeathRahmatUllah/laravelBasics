f310fcbb3ad1118f54715e7bb1a97b6052701248
J Rahmat Ullah <48548216+JeathRahmatUllah@users.noreply.github.com>
2023-10-26 17:57:39 +0600

    Update web.php

--- web.php
+++ web.php
@@ -1,1 +1,44 @@
+//Basic Routing
 
+use Illuminate\Support\Facades\Route;
+ 
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+//The Default Route Files
+
+use App\Http\Controllers\UserController;
+ 
+Route::get('/user', [UserController::class, 'index']);
+
+//Available Router Methods
+
+Route::get($uri, $callback);
+Route::post($uri, $callback);
+Route::put($uri, $callback);
+Route::patch($uri, $callback);
+Route::delete($uri, $callback);
+Route::options($uri, $callback);
+
+//Dependency Injection
+use Illuminate\Http\Request;
+ 
+Route::get('/users', function (Request $request) {
+    // ...
+});
+
+//<form method="POST" action="/profile">
+    @csrf
+    ...
+</form>
+
+//Redirect Routes
+
+Route::redirect('/here', '/there');
+
+//view routes
+
+Route::view('/welcome', 'welcome');
+ 
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
